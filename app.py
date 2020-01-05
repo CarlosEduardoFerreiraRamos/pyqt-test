b65f8f5bb623280b7572eca873587523895e0cda
Carlos Ramos <carlosefrfloresta@gmail.com>
2020-01-05 16:05:53 -0300

    Add changes in the global JSON parser, with the intent to test'it for new ways to apply models

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from waitress import serve
 
 """ Flask moduels """
 from flask import Flask, make_response
+from flask.json import JSONEncoder, JSONDecoder
 from flask_restful import Api
 
 """internal modules """
@@ -23,6 +24,34 @@ from util import get_command_prop
 ms = MockService()
 app = Flask(__name__)
 api = Api(app)
+
+class CustomJSONEncoder(JSONEncoder):
+    """ Do nothing custom json encoder """
+    def __init__(self, *args, **kargs):
+        super()
+
+    def default(self, obj):
+        print(' JSONEncoder',obj)
+        return super().default(obj)
+
+
+class CustomJSONDecoder(JSONDecoder):
+    """ Do nothing custom json decoder """
+
+    def decode(self, d):
+        print(' JSONDecoder',d)
+        return super().decode(d)
+
+app.json_encoder = CustomJSONEncoder
+app.json_decoder = CustomJSONDecoder 
+
+@app.url_defaults
+def add_language_code(*args):
+    print(' url_defaults',args)
+
+@app.url_value_preprocessor
+def pull_lang_code(*args):
+    print(' url_value_preprocessor',args)
 
 @api.representation('application/json')
 def output_json(data, code, headers=None):
